085fa7e1d5756ab8926779a020f3cf35136b1806
Filipe Mendes da Rocha Lopes <filipemendespi@gmail.com>
2017-12-10 12:59:02 -0300

    Removing extre lines (auction_spec) and validation format (auction)

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -10,13 +10,6 @@ class Auction < ApplicationRecord
             :current_value,
             :ending_value,
             numericality: { greater_than: 0 }
-  validates :start_value,
-            :current_value,
-            :ending_value,
-            format: {
-              with: /\A\d{1,10}\.\d{1,2}\z/,
-              message: I18n.t('errors.messages.wrong_format_price')
-            }, allow_blank: true
   validate :lower_ending_value
 
   private
--- spec/models/auction_spec.rb
+++ spec/models/auction_spec.rb
@@ -1,5 +1,3 @@
-require 'rails_helper'
-
 RSpec.describe Auction, type: :model do
   let(:auction) { FactoryBot.build(:auction) }
 
