992e39aef06f1c88ea5ed44f912de3bc6feaa6e9
Filipe Mendes da Rocha Lopes <filipemendespi@gmail.com>
2017-12-10 13:12:11 -0300

    Adding presence validations to bid_spec

--- spec/models/bid_spec.rb
+++ spec/models/bid_spec.rb
@@ -4,6 +4,10 @@ RSpec.describe Bid, type: :model do
   it { should belong_to(:user) }
   it { should belong_to(:auction) }
 
+  it { should validate_presence_of(:user) }
+  it { should validate_presence_of(:auction) }
+  it { should validate_presence_of(:value) }
+
   it{ should validate_numericality_of(:value).is_greater_than(0) }
 
   it 'value has invalid format' do
