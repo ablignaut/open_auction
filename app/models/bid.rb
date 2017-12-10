8513685cf00ccded867bcca54094f53e5f582d38
Filipe Mendes da Rocha Lopes <filipemendespi@gmail.com>
2017-12-10 13:11:14 -0300

    Adding allow_blank to validations

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -2,8 +2,11 @@ class Bid < ApplicationRecord
   belongs_to :user
   belongs_to :auction
 
-  validates :value,
+  validates :user,
+            :auction,
+            :value,
             presence: true
   validates :value,
-            numericality: { greater_than: 0 }
+            numericality: { greater_than: 0 },
+            allow_blank: true
 end
