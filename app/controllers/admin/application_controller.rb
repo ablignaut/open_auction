f47d55decabee55cb5475f4b271f4b85d1ac9eb6
Filipe Mendes da Rocha Lopes <filipemendespi@gmail.com>
2017-12-11 06:12:44 -0300

    Fixing logout

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,6 +1,6 @@
 module Admin
   class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
+    protect_from_forgery with: :null_session
     before_action :authenticate_admin_user!
 
     layout 'admin'
--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -19,5 +19,9 @@ module Admin
     def configure_sign_in_params
       devise_parameter_sanitizer.permit(:sign_in)
     end
+
+    def after_sign_out_path_for(resource_or_scope)
+      admin_root_path
+    end
   end
 end
