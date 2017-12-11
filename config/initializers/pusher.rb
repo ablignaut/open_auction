75f2a86b30d69410dc4b0968c764301f9563cae8
Filipe Mendes da Rocha Lopes <filipemendespi@gmail.com>
2017-12-10 21:00:44 -0300

    Configuring Pusher keys to development

---
+++ config/initializers/pusher.rb
@@ -0,0 +1,8 @@
+require 'pusher'
+
+Pusher.app_id = '442844'
+Pusher.key = '78754a64770633d78f3f'
+Pusher.secret = '18145f26632311ff9afd'
+Pusher.cluster = 'us2'
+Pusher.logger = Rails.logger
+Pusher.encrypted = true
